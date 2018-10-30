e203c48b26b3552d748bc502bd1f15b0c15996df
abcapindo <acapindo0123@utexas.edu>
2018-10-30 18:34:24 -0500

    Added ability to download chunk of data with a given url and given number of wanted chunks

--- app.js
+++ app.js
@@ -1,57 +1,98 @@
-const requestPromise = require('request-promise');
-const url = 'http://f39bf6aa.bwtest-aws.pravala.com/384MB.jar';
-const arg = process.argv[2];
+const request = require('request');
+const fs = require('fs');
+const url = require("url");
+const path = require("path");
 
-const ranges = ['0-1023', '1024-2047', '2048-3071', '3072-4095'];
-let data;
-let compare;
-let count = 1;
-
-function promise(range) {
-    var options = {
-        uri: arg,
-        headers: {
-            'Range': 'bytes=' + range
-        }
-    }
-
-    requestPromise(options)
-        .then(function (resp) {
-            data += resp;
-            console.log(count);
-            count += 1;
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-}
-
-function fourMB() {
-    var options = {
-        uri: arg,
-        headers: {
-            'Range': 'bytes=0-4095'
-        }
-    }
-    requestPromise(options)
-        .then(function (resp) {
-            compare += resp;
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-}
+const apiUrl = process.argv[2];
+let numChunks = process.argv[3] != undefined ? process.argv[3] : 4;
 
 const promises = [];
+const ranges = [];
 
-ranges.forEach(function (element) {
-    promises.push(promise(element));
-});
+/**
+ * Creates string ranges of 1 MB for number of given chunks. Used in http calls.
+ * @param {number} numChunks 
+ */
+function createRanges(numChunks) {
+    let start = 0;
+    let end = 1048575;
+    for (let i = 0; i < numChunks; i++) {
+        ranges.push(start + '-' + end);
+        start += 1048576;
+        end += 1048576;
+    }
+}
 
-promises.push(fourMB);
+/**
+ * Creates http call.
+ * @param {String Array} range 
+ */
+function promise(range) {
+    console.log('Creating Promise: ' + range);
+    var options = {
+        uri: apiUrl,
+        encoding: null,
+        headers: {
+            'Range': 'bytes=' + range,
+            'Accept-Encoding': 'gzip, deflate'
+        }
+    }
+    return new Promise(function (resolve, reject) {
+        request.get(options, function (err, resp, body) {
+            console.log('Promise' + resp.headers['content-range']);
+            if (err) {
+                reject(err);
+            } else {
+                resolve(Buffer.from(body));
+            }
+        });
+    });
+}
 
-Promise.all(promises).then(function () {
-    console.log(data == compare);
-});
+/**
+ * Runs application
+ */
+function main() {
+    const fileName = path.basename(url.parse(apiUrl).pathname);
+    createRanges(numChunks);
 
-console.log(arg);
+    fs.unlink(fileName, function () {
+        console.log('Deleted File');
+    })
+
+    ranges.forEach(function (element) {
+        promises.push(promise(element));
+    });
+
+    Promise.all(promises).then(function (values) {
+        values.forEach(function (data) {
+            fs.appendFileSync(fileName, data, 'binary', function (err) {
+                if (err) throw err;
+            });
+        });
+    });
+}
+
+function test() {
+    const options = {
+        uri: apiUrl,
+        headers: {
+            'Range': 'bytes=0-4194303',
+            'Accept-Encoding': 'gzip, deflate'
+        },
+        encoding: null
+    };
+
+    request.get(options, function (err, resp, body) {
+        if (err) {
+            console.log(err);
+        } else {
+            fs.writeFile('./nani.jar', body, 'binary', function (err) {
+                if (err) throw err;
+                console.log('Done');
+            })
+        }
+    });
+}
+
+main();
